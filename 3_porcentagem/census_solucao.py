41e1e3e952e81ae8ee7c12622ae5ac50d6ee0c59
Carol Caires <carolbazarello@gmail.com>
2020-11-11 08:00:49 -0300

    solução do professor exercicio percentuais

--- 3_porcentagem/census_solucao.py
+++ 3_porcentagem/census_solucao.py
@@ -1,1 +1,22 @@
-# todo: solucao do professor
+"""
+    Solução do exercício feita pelo professor.
+    Mantive a minha solução no arquivo census.py, pois dá uma visão diferente
+    e a tabela final é de visualização mais fácil da ideia
+"""
+
+import pandas as pd
+
+dataset = pd.read_csv('census.csv')
+
+dataset2 = dataset[['income', 'education']]
+dataset3 = dataset2.groupby(['education', 'income'])['education'].count()
+
+menos50k = dataset3[' Bachelors', ' <=50K']
+mais50k = dataset3[' Bachelors', ' >50K']
+total = menos50k + mais50k
+
+mais50kPercent = (mais50k / total) * 100
+menos50kPercent = (menos50k / total) * 100
+
+print('%.2f por cento das pessoas ganham menos ou igual 50k.\n%.2f por cento ganham mais que 50k.\nTotal de pessoas: %d'
+      % (menos50kPercent, mais50kPercent, total))
