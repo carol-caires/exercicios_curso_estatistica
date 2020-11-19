e1d51caa86ec4a7ac86349141e13d622efc25e92
Carol Caires <carolbazarello@gmail.com>
2020-11-19 08:51:22 -0300

    quartis em dados agrupados

--- 6_posicao_dispersao/estatisticas_dados_agrupados.py
+++ 6_posicao_dispersao/estatisticas_dados_agrupados.py
@@ -7,7 +7,6 @@ import pandas as pd
     Fi = somatório acumulado de elementos nas classes anteriores e atual
 '''
 
-
 dados = {'inferior': [150, 154, 158, 162, 166, 170],
          'superior': [154, 158, 162, 166, 170, 174],
          'fi': [5, 9, 11, 7, 5, 3]}
@@ -20,8 +19,8 @@ dataset['Fi'] = 0
 frequencia_acumulada = []
 somatorio = 0
 for linha in dataset.iterrows():
-  somatorio += linha[1][2]
-  frequencia_acumulada.append(somatorio)
+    somatorio += linha[1][2]
+    frequencia_acumulada.append(somatorio)
 
 dataset['Fi'] = frequencia_acumulada
 
@@ -46,3 +45,27 @@ def get_estatisticas(dataframe):
 
 
 print(get_estatisticas(dataset))
+
+
+# Quartil - mediana a 25% e 75%
+def get_quartil(dataframe, q1=True):
+    if q1 == True:
+        fi_4 = dataframe['fi'].sum() / 4
+    else:
+        fi_4 = (3 * dataframe['fi'].sum()) / 4
+
+    limite_inferior, frequencia_classe, id_frequencia_anterior = 0, 0, 0
+    for linha in dataframe.iterrows():
+        limite_inferior = linha[1][0]
+        frequencia_classe = linha[1][2]
+        id_frequencia_anterior = linha[0]
+        if linha[1][5] >= fi_4:
+            id_frequencia_anterior -= 1
+            break
+    Fi_anterior = dataframe.iloc[[id_frequencia_anterior]]['Fi'].values[0]
+    q = limite_inferior + ((fi_4 - Fi_anterior) * 4) / frequencia_classe
+
+    return q
+
+
+print(get_quartil(dataset), get_quartil(dataset, q1=False))
